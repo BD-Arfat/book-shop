5f46e52becd4e4cc7f1adf6e8e65b2682608e180
Ariful islam <129893986+BD-Arfat@users.noreply.github.com>
2023-08-25 05:20:30 +0600

    Update AddProducts.js

--- src/Components/AddProducts/AddProducts.js
+++ src/Components/AddProducts/AddProducts.js
@@ -6,6 +6,7 @@ const AddProducts = () => {
         <div>
             <h1 className='font-bold text-3xl pt-40'>Add Products</h1>
             <div>
+        // addForm Components
                 <AddForm/>
             </div>
         </div>
