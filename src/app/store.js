e9d4e57e81a20f31e441cdb9f779a6eeb363ff04
Ariful islam <129893986+BD-Arfat@users.noreply.github.com>
2023-08-28 08:53:13 +0600

    Update store.js

--- src/app/store.js
+++ src/app/store.js
@@ -5,8 +5,11 @@ import filterReducer from '../features/filter/filterSlice';
 
 export const store = configureStore({
   reducer: {
+    // from booksSlice
     books: booksReducer,
+    // from bookSliec
     book: bookReducer,
+    // from filterSlice
     filter : filterReducer
   },
 });
