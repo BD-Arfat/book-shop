c152bf9409e71c06031a6a9ebd3c9e19a8b46e21
Ariful islam <129893986+BD-Arfat@users.noreply.github.com>
2023-09-07 22:01:24 +0600

    Update BookApi.js

--- src/features/Book/BookApi.js
+++ src/features/Book/BookApi.js
@@ -1,6 +1,9 @@
 import axios from '../../Components/utils/axios';
 
+// book api 
 export const getBook = async () => {
     const res = await axios.get('/product');
     return res.data;
   }
+
+// end book api
